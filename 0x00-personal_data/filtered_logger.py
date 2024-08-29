c86cfd6e90c2ef9376398b6f6aa6a3d5d5f7a914
thevorgx <kh.elallali@gmail.com>
2024-08-29 23:21:57 +0100

    update

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -35,9 +35,9 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """place holder for now"""
-    username = os.getenv('PERSONAL_DATA_DB_USERNAME')
-    password = os.getenv('PERSONAL_DATA_DB_PASSWORD')
-    host = os.getenv('PERSONAL_DATA_DB_HOST')
+    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', 'localhost')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', '')
     db_name = os.getenv('PERSONAL_DATA_DB_NAME')
     db_connect = mysql.connector.connect(host=host, user=username,
                                          password=password, database=db_name)
