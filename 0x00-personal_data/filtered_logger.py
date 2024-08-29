e70ffefd0c04b2ed4d6717f2a141c89f4b434e3a
thevorgx <kh.elallali@gmail.com>
2024-08-29 20:55:42 +0100

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Regex-ing given fields"""
+import logging
 import re
 from typing import List
 
@@ -11,3 +12,24 @@ def filter_datum(fields: List[str], redaction: str,
         ragex_patt = rf'({field}=)[^{separator}]*'
         message = re.sub(ragex_patt, rf'\1{redaction}', message)
     return (message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Redacts sensitive info and formats the log record for output"""
+        original_message = record.getMessage()
+        redacted_message = filter_datum(self.fields, self.REDACTION,
+                                        original_message, self.SEPARATOR)
+        record.msg = redacted_message
+        return super().format(record)
