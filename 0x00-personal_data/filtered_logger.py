104d445ccd18ddb585dd0de1466b716f98d4ead7
thevorgx <kh.elallali@gmail.com>
2024-08-29 03:04:56 +0100

    task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""Regex-ing given fields"""
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """Obfuscate fields using a ragex pattern(field + separator)"""
+    for field in fields:
+        ragex_patt = rf'({field}=)[^{separator}]*'
+        message = re.sub(ragex_patt, rf'\1{redaction}', message)
+    return (message)
