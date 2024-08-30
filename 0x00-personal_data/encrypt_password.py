3c7925d422573302f4d625382d987be5a3f3e721
thevorgx <kh.elallali@gmail.com>
2024-08-30 01:57:08 +0100

    test requirements

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,6 +6,5 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """take an str password convert it to bytes and hash it"""
     pw_bytes = password.encode()
-    hashed_pw = bcrypt.hashpw(pw_bytes,
-                              bcrypt.gensalt())
+    hashed_pw = bcrypt.hashpw(pw_bytes, bcrypt.gensalt())
     return hashed_pw
