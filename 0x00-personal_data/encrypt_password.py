cb743f30fe35726cb00a5569f2e8e7f8fee2f2eb
thevorgx <kh.elallali@gmail.com>
2024-08-30 02:06:28 +0100

    task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 """hash password module"""
-import bcrypt
+from bcrypt import hashpw, gensalt, checkpw
 
 
 def hash_password(password: str) -> bytes:
     """take an str password convert it to bytes and hash it"""
     pw_bytes = password.encode()
-    hashed_pw = bcrypt.hashpw(pw_bytes, bcrypt.gensalt())
+    hashed_pw = hashpw(pw_bytes, gensalt())
     return hashed_pw
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """check if hashed_pw is pw"""
+    pw_bytes = password.encode()
+    if checkpw(pw_bytes, hashed_password):
+        return True
+    else:
+        return False
