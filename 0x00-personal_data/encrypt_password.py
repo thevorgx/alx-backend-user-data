ebcf2ce6318358e6b7a00224a287f3cda9f1ecae
thevorgx <kh.elallali@gmail.com>
2024-08-30 01:56:09 +0100

    update requirements

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-"""hash pw module"""
+"""hash password module"""
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """take a str password convert it to bytes and hash it"""
+    """take an str password convert it to bytes and hash it"""
     pw_bytes = password.encode()
-    hashed_pw = bcrypt.hashpw(pw_bytes, bcrypt.gensalt())
-    return(hashed_pw)
+    hashed_pw = bcrypt.hashpw(pw_bytes,
+                              bcrypt.gensalt())
+    return hashed_pw
