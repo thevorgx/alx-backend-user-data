525fc170483a9f4efbf475ab3bee29b59bbddc39
thevorgx <kh.elallali@gmail.com>
2024-09-13 03:43:22 +0100

    task 4

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+"""Auth module"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """Hash a password using bcrypt"""
+    hashed_pw = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_pw
