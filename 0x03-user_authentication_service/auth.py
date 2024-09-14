c93b55ad74ac6f0ec3e06b78259b0db1f9219b99
thevorgx <kh.elallali@gmail.com>
2024-09-14 03:43:56 +0100

    hakai

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -73,3 +73,7 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Hakai the user session by setting session ID to None"""
+        self._db.update_user(user_id, session_id=None)
